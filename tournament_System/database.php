32f7fa8ad1a6d6d1a7ea6755e1fec001599ff4bc
archerchess01-coder <archerchess01@gmail.com>
2026-04-27 13:24:36 +0530

    Initial database connection setup

---
+++ tournament_System/database.php
@@ -0,0 +1,14 @@
+<?php
+// Database connection
+
+$host = 'localhost';
+$db = 'tournament';
+$user = 'root';
+$pass = '';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+?>
